9390b2bb5be7841e3c92c79db396fd29c43b9417
Jia Wang <amber.jiawang@gmail.com>
2024-11-30 17:52:43 +0100

    js nav link set active styles, fix cta, animation speakers

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,5 +10,13 @@
  * Write any other JavaScript below
  */
 
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
 
+const url = document.location.href;
+
+let key = "index";
+if (url.includes("participants")) key = "participants";
+else if (url.includes("news")) key = "news";
+else if (url.includes("contact")) key = "contact";
+
+document.querySelector(`#nav-link-${key}`).classList.add("active");
